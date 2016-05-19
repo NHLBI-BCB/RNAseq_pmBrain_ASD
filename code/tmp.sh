a0e4ff7b315f9f0955de0f7496da4dcd3983a20a
Jill Haney <jillhaney21@ucla.edu>
2016-05-18 17:20:31 -0700

    once more

--- code/tmp.sh
+++
@@ -1,9 +0,0 @@
-SOURCEDIR=/hp_shares/mgandal/projects/BrainGVEX/source
-
-cd $SOURCEDIR
-
-while read direc;do
-
-ln -s /hp_shares/mgandal/projects/BrainGVEX/data/case-control/RAW/${direc}/* /hp_shares/mgandal/projects/BrainGVEX/data/temp/
-
-done<samples_redo_fromAlgn.txt
